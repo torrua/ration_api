26a6986cca0041510741a04251dff3107c89683f
torrua <torrua@users.noreply.github.com>
2024-06-15 20:59:57 +0700

    Update ProductCategory schema

--- src/api/routers/product_category.py
+++ src/api/routers/product_category.py
@@ -3,13 +3,14 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
+from .utils import create_item, read_items, delete_item
 from ..models import User, ProductCategory as ItemORM
-from ..schemas.product_category import ProductCategoryInDBBase as ItemInDBBase
 from ..schemas.product_category import ProductCategory as Item
 from ..schemas.product_category import (
     ProductCategoryCreate as ItemCreate,
     ProductCategoryUpdate as ItemUpdate,
 )
+from ..schemas.product_category import ProductCategoryInDBBase as ItemInDBBase
 from ...auth.base_config import current_user
 from ...database import get_async_session
 
@@ -26,20 +27,15 @@ async def create_product_category(
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    new_item = ItemORM(**item.dict(), user_id=user.id)
-    session.add(new_item)
-    await session.commit()
-    await session.refresh(new_item)
-    return new_item
+    return await create_item(item=item, class_orm=ItemORM, session=session, user=user)
 
 
-@router.get("/")
+@router.get("/", response_model=list[ItemInDBBase])
 async def read_product_categories(
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    items = await session.execute(select(ItemORM).where(ItemORM.user_id == user.id))
-    return [ItemInDBBase.model_validate(item) for item in items.scalars().all()]
+    return await read_items(class_orm=ItemORM, session=session, user=user)
 
 
 @router.get("/{item_id}")
@@ -79,10 +75,6 @@ async def delete_product_category(
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    item = await session.execute(
-        select(ItemORM).where(ItemORM.id == item_id, ItemORM.user_id == user.id)
+    return await delete_item(
+        item_id=item_id, class_orm=ItemORM, session=session, user=user
     )
-    deleted = item.scalars().first()
-    await session.delete(deleted)
-    await session.commit()
-    return deleted
