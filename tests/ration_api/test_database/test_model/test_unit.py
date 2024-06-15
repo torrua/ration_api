bcf71a0bced16f7040693cb9fb3a9a3cb8bda468
torrua <torrua@users.noreply.github.com>
2024-06-15 15:40:49 +0700

    Add Unit tests

--- tests/ration_api/test_database/test_model/test_unit.py
+++ tests/ration_api/test_database/test_model/test_unit.py
@@ -1,8 +1,24 @@
+from pytest import raises
+from sqlalchemy.exc import IntegrityError
+
 from tests.conftest import *
 
 
 @pytest.mark.usefixtures("filled_session")
 class TestUnit:
 
-    def test__repr__(self, filled_session):
-        pass
+    @staticmethod
+    def unit(filled_session):
+        return (
+            filled_session.query(Unit)
+            .filter(Unit.title == gr_data.get("title"))
+            .first()
+        )
+
+    def test_delete_unit(self, filled_session):
+        unit = self.unit(filled_session)
+        assert unit is not None
+
+        with raises(IntegrityError):
+            filled_session.delete(unit)
+            filled_session.commit()
