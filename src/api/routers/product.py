3165673c9717ea687f326a160ea3a5cf4c288bc3
torrua <torrua@users.noreply.github.com>
2024-06-22 11:48:54 +0700

    Product route

--- src/api/routers/product.py
+++ src/api/routers/product.py
@@ -1,14 +1,13 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .utils import create_item, read_items, read_item, delete_item
+from .utils import create, read_all, read_one, delete, update
 from ..models import User, Product as ItemORM
 from ..schemas.product import (
     Product as Item,
     ProductCreate as ItemCreate,
-    ProductBase as ItemBase,
     ProductUpdate as ItemUpdate,
+    ProductInDB as ItemInDB,
 )
 from ...auth.base_config import current_user
 from ...database import get_async_session
@@ -20,51 +19,31 @@ router = APIRouter(
 )
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=ItemBase)
+@router.post("/", response_model=Item, status_code=status.HTTP_201_CREATED)
 async def create_product(
     item: ItemCreate,
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    return await create_item(item=item, class_orm=ItemORM, session=session, user=user)
+    return await create(data=item, class_orm=ItemORM, session=session, user=user)
 
 
-async def check_item_ownership(
-    item_id: int,
-    user: User = Depends(current_user),
-    session: AsyncSession = Depends(get_async_session),
-) -> ItemORM:
-    item_db = await session.execute(select(ItemORM).where(ItemORM.id == item_id))
-    item_db = item_db.scalars().first()
-    if item_db is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    if item_db.user_id != user.id:
-        raise HTTPException(
-            status_code=403, detail="Not authorized to update this item"
-        )
-    return item_db
-
-
-@router.put("/{item_id}", response_model=ItemBase)
+@router.put("/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
 async def update_product(
+    item_id: int,
     item_update: ItemUpdate,
-    item_db: ItemORM = Depends(check_item_ownership),
     session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user),
 ):
-    for key, value in item_update.dict(exclude_unset=True).items():
-        setattr(item_db, key, value)
-    session.add(item_db)
-    await session.commit()
-    await session.refresh(item_db)
-    return item_db
+    return await update(item_id=item_id, data=item_update, class_orm=ItemORM, session=session, user=user)
 
 
-@router.get("/", response_model=list[Item], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=list[ItemInDB], status_code=status.HTTP_200_OK)
 async def read_products(
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    return await read_items(class_orm=ItemORM, session=session, user=user)
+    return await read_all(class_orm=ItemORM, session=session, user=user)
 
 
 @router.get("/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
@@ -73,29 +52,17 @@ async def read_product(
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    return await read_item(
-        item_id=item_id, class_orm=ItemORM, session=session, user=user
+    return await read_one(
+        item_id=item_id, class_orm=ItemORM, session=session, user=user,
     )
 
 
-@router.delete("/{item_id}", response_model=ItemBase, status_code=status.HTTP_200_OK)
+@router.delete("/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(
     item_id: int,
     session: AsyncSession = Depends(get_async_session),
     user: User = Depends(current_user),
 ):
-    """
-    Deletes a product by its ID.
-    Args:
-        item_id (int): The ID of the product to delete.
-        session (AsyncSession, optional): The database session.
-        Defaults to Depends(get_async_session).
-        user (User, optional): The authenticated user. Defaults to Depends(current_user).
-    Returns:
-        None: If the product is successfully deleted.
-    Raises:
-        HTTPException: If the product is not found or the user is not authorized to delete it.
-    """
-    return await delete_item(
+    return await delete(
         item_id=item_id, class_orm=ItemORM, session=session, user=user
     )
