fed02d44520528db85e0db0e356ec4cdd7e42852
torrua <torrua@users.noreply.github.com>
2024-08-07 07:43:34 +0700

    Add DB_RULE variable for URI

--- src/database.py
+++ src/database.py
@@ -15,9 +15,10 @@ from .config import (
     DB_PASS,
     DB_PORT,
     DB_USER,
+    DB_RULE,
 )
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}{DB_RULE}"
 
 engine = create_async_engine(
     DATABASE_URL,
